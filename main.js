8c977478315fcb19aea21e2aaefdf4b6166c7671
Miver <amarantagrf@gmail.com>
2024-07-13 10:55:27 +0800

    new file:   asset/turret.png 	modified:   main.js

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ import {init_unity_academy_3d, init_unity_academy_2d, set_start, set_update,
     set_angular_velocity, set_mass, set_use_gravity ,set_velocity,
     on_collision_enter, on_collision_stay, on_collision_exit,
     get_main_camera_following_target, vector3, get_x, get_y, vector_difference,
-    add_vectors, scale_vector, normalize, debug_log, get_z, magnitude
+    add_vectors, scale_vector, normalize, debug_log, get_z, magnitude,
+    remove_collider_components
 }
     from "unity_academy";
 
@@ -31,6 +32,18 @@ function my_debug(gameObject) {
 
 ///////////////////////////////////////////////////////
 //<----------------------------------------------------
+// image asset
+const PlayerImageURL = "https://raw.githubusercontent.com/Amaranterre/SICP/main/asset/player.png";
+
+const PlayerBulletImageURL = "https://raw.githubusercontent.com/Amaranterre/SICP/main/asset/play_bullet.png";
+const playerBulletImageAngle = vector3(0, 0, 135);
+
+//--------------------------------------------------->//
+///////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////
+//<----------------------------------------------------
 // const related
 
 const x_boundary = 15;
@@ -40,6 +53,11 @@ const direction_left = vector3(-1, 0, 0);
 const direction_right = vector3(1, 0, 0);
 const direction_up = vector3(0, 1, 0);
 const direction_down = vector3(0, -1, 0);
+
+let cur_time = 0;
+function get_game_time() {
+    return cur_time;
+}
 
 //--------------------------------------------------->//
 ///////////////////////////////////////////////////////
@@ -51,7 +69,8 @@ const direction_down = vector3(0, -1, 0);
 
 function is_out_boundary(gameObject) {
     const pos = get_position(gameObject);
-    return math_abs(x_of(pos)) > x_boundary || math_abs(y_of(pos)) > y_boundary
+    return math_abs(get_x(pos)) > x_boundary || 
+            math_abs(get_y(pos)) > y_boundary;
 }
 
 //--------------------------------------------------->//
@@ -81,7 +100,50 @@ function is_same_vector(vec1, vec2) {
 //<----------------------------------------------------
 // bullet related 
 
-// function createBullet
+//create a bullet facing $angular at $pos, with $velocity
+function getPlayerBulletCreator(time_gap) {
+    const cur_time = get_game_time();
+    let last_time = cur_time;
+    
+    
+    return (pos, angle, velocity, scale) => {
+        // debug_log("last time: " + stringify(last_time) + " cur_time: " + stringify(get_game_time()));
+        if(get_game_time() - last_time < time_gap) {
+            // debug_log("not shoot");
+            return null;
+        } else {
+        
+            const cur_bullet = instantiate_sprite(PlayerBulletImageURL);
+    
+            set_start(cur_bullet, (gameObject) => {
+                apply_rigidbody(gameObject);
+                remove_collider_components(gameObject); // very important !!
+                set_use_gravity(gameObject, false);
+            
+        set_rotation_euler(gameObject, 
+                    vector_difference(angle, playerBulletImageAngle)); // modify the image angle
+                set_velocity(gameObject, velocity);
+                set_position(gameObject, pos);
+                
+                set_scale(gameObject, scale);
+            });
+            
+            set_update(cur_bullet, (gameObject) => {
+                if(is_out_boundary(gameObject)) { // cross the boundary of map, destroy
+                    destroy(gameObject);
+                    debug_log("destroy myself 0_0");
+                        
+                }
+            });
+            
+            last_time = get_game_time();
+        }
+    };
+    
+    
+    
+    
+}
 
 
 //--------------------------------------------------->//
@@ -92,7 +154,7 @@ function is_same_vector(vec1, vec2) {
 //<----------------------------------------------------
 // player related
 
-const player = instantiate_sprite("https://raw.githubusercontent.com/Amaranterre/SICP/main/asset/player.png");
+const player = instantiate_sprite(PlayerImageURL);
 let player_speed = 3;
 
 function get_player_move_direction() {
@@ -166,10 +228,28 @@ function start_player(gameObject){
     apply_rigidbody(gameObject);
     set_use_gravity(gameObject, false);
 }
+
+const bullet_creator = getPlayerBulletCreator(0.3); //set shoot gap
+
 function update_player(gameObject){
+    cur_time = cur_time + delta_time();
+    
     player_move(gameObject);
     
-    my_debug(gameObject);
+    
+    
+    if(get_key("Q")){
+        const position = get_position(gameObject);
+        // const position = vector3(0, 0, 0);
+        const velocity = vector3(5, 0, 0);
+        const angle = vector3(0, 0, 0);
+        const scale = vector3(2, 2, 0); //set bullet size
+        
+        
+        bullet_creator(position, angle, velocity, scale);
+    } 
+    
+    // my_debug(gameObject);
 
 }
 
